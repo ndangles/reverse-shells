30f4ff4f4baaa0ea758d227b60e91116bb173816
Nicholas Dangles <ncd275@users.noreply.github.com>
2017-06-24 21:06:19 -0500

    added some control

--- twilio-shell.js
+++ twilio-shell.js
@@ -8,15 +8,20 @@ const twilio 			= require('twilio')(accountSid, authToken);
 const MessagingResponse 	= require('twilio').twiml.MessagingResponse;
 
 app.get('/api/sms', function(req, res){
-	
-	cmd.get(req.query.Body, function(err, data){
-		//https://www.twilio.com/docs/guides/how-to-receive-and-reply-in-node-js
-		const twiml = new MessagingResponse();
-		twiml.message(data);
-		res.writeHead(200, {'Content-Type': 'text/xml'});
-		res.end(twiml.toString());
+	if(req.query.From == '+12223334444') { //your phone number to make sure no one else but you can send commands
+		cmd.get(req.query.Body, function(err, data){
+			//https://www.twilio.com/docs/guides/how-to-receive-and-reply-in-node-js
+			const twiml = new MessagingResponse();
+			twiml.message(data);
+			res.writeHead(200, {'Content-Type': 'text/xml'});
+			res.end(twiml.toString());
 
-	});
+		});
+	} else {
+		
+		//do nothing
+			
+	}
 });
 
 ngrok.connect(7672, function(err, url){
